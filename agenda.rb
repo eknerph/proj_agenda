82b1ba99e0496d4e4f755f1ccf979fceea499ff2
eknerph <eknerph@gmail.com>
2024-10-27 00:19:13 -0300

    Adicionado código para adicionar contato na opção 2.

--- agenda.rb
+++ agenda.rb
@@ -3,26 +3,29 @@
 # Poderemos ver todos os contatos registrados ou somente um contato;
 
 @agenda = [
-  {nome: "Maria", telefone: "21 9999-3333"}
+  {nome: "Maria", telefone: "21 9999-3333"},
   {nome: "Julice", telefone: "21 98567-5580"}
 ]
 
 def todos_contatos
-  @ agenda.each do |contato|
+  @agenda.each do |contato|
     puts "#{contato[:nome]} - #{contato[:telefone]}"
   end
   puts "-----------------------------------------"
 end
 
 def adicionar_contato
-  print "Nome:"
+  print "Nome: "
   nome = gets.chomp
   print "Telefone: "
   telefone = gets.chomp
 
+  @agenda << {nome: nome, telefone: telefone}
+end
+
 loop do
 
-puts "1. Contatos\n2. Adicinar Contato\n3. Editar Contato\n4. Remover Contato\n5. Sair\n"
+puts "1. Contatos\n2. Adicinar Contato\n3. Ver COntato\n4. Editar Contato\n5. Remover Contato\n0. Sair\n"
   codigo = gets.chomp.to_i
   
   case
@@ -32,7 +35,8 @@ puts "1. Contatos\n2. Adicinar Contato\n3. Editar Contato\n4. Remover Contato\n5
   when codigo == 1    
     todos_contatos   
   when codigo == 2
-    adicinar_contato
+    adicionar_contato
+
   end
 
 end
