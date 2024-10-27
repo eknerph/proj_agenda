5257fd46c72b0d9e707bf6be58be1cf66332c70b
eknerph <eknerph@gmail.com>
2024-10-26 23:43:19 -0300

    Adicinado código de exemplo para variável agenda em formaato de array

--- agenda.rb
+++ agenda.rb
@@ -1,4 +1,23 @@
-# Adcionar, Editar e remover Contato;
+# Adicionar, Editar e remover Contato;
 # Os Contatos terão as seguintes informações: nome e telefone;
-#Poderemos ver todos os contatos registrados ou somente um contato;
+# Poderemos ver todos os contatos registrados ou somente um contato;
 
+@agenda = [
+  {nome: "Maria", telefone: "21 9999-3333"}
+  {nome: "Julice", telefone: "21 98567-5580"}
+]
+
+
+
+loop do
+
+puts "1. Contatos\n2. Adicinar Contato\n3. Editar Contato\n4. Remover Contato\n5. Sair\n"
+  codigo = gets.chomp.to_i
+  
+  case
+  when codigo == 0
+    puts "Até mais!"
+    break         
+  end
+
+end
